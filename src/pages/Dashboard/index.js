88843dbbf67b16ae0af5d38d1a109ac853ac6cb9
NoeliaRios <noelia.rivers.1887@gmail.com>
2020-10-16 02:30:50 -0300

    fetch

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useLocation} from 'react-router-dom'
 
 function Dashboard() {
   const location = useLocation();
-  console.log(location.hash);
   const [fullHash] = location.hash.split('&');
-  // const accessToken = fullHash.slice(1, fullHash.length);
-  const accessToken = fullHash.split('=');
+  // const accessToken = fullHash.split('=');
+  // console.log(accessToken[1]);
+  const [,accessToken] = fullHash.split('=');
+  console.log(accessToken);
 
-  console.log(accessToken[1]);
+  const userDataApi = 'https://api.spotify.com/v1/me/';
+  const userPlaylistsApi = 'https://api.spotify.com/v1/me/playlists';
+  const userRecentlyPlayedApi = 'https://api.spotify.com/v1/me/player/recently-played';
 
 
+    const [userData, setUserData] = useState();
+  const [userPlaylists, setUserPlaylists] = useState();
+    const [userRecentlyPlayed, setUserRecentlyPlayed] = useState();
+  
+
+    useEffect(()=> {
+        fetchData(userDataApi, setUserData);
+      fetchData(userPlaylistsApi, setUserPlaylists);
+        fetchData(userRecentlyPlayedApi, setUserRecentlyPlayed);
+      
+    },[accessToken]);
+
+    async function fetchData(url, setter) {
+        const data = await fetch(
+            url,
+            {
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            }
+            })
+        const dataJson = await data.json();
+        setter(dataJson)
+    }
+
+  console.log(userData);
+  console.log(userPlaylists);
+  console.log(userRecentlyPlayed);
+  
 
   return <div>Soy un Dashboard</div>
 }
